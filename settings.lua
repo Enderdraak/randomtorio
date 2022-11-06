3bb953d7b44fb407b5511e652b54030388613e66
Enderdraak <Endedraak@gmail.com>
2022-11-06 10:07:59 +0100

    fixed explanations

--- settings.lua
+++ settings.lua
@@ -1,15 +1,24 @@
 data:extend({
     {
         type = "bool-setting",
-        name = "randomtorio-results",
+        name = "randomtorio-ingredients",
         setting_type = "startup",
-        default_value = false
+        default_value = true,
+        order = "a-a"
     },
     {
         type = "bool-setting",
-        name = "randomtorio-ingredients",
+        name = "randomtorio-results",
         setting_type = "startup",
-        default_value = true
+        default_value = false,
+        order = "a-b"
+    },
+    {
+        type = "int-setting",
+        name = "randomtorio-randomseed",
+        setting_type = "startup",
+        default_value = 47605,
+        order = "b-a"
     },
     {
         type = "string-setting",
@@ -17,18 +26,14 @@ data:extend({
         setting_type = "startup",
         allowed_values = {"disable", "normal-only", "expensive-only", "normal-and-expensive"},
         default_value = "disable",
-    },
-    {
-        type = "int-setting",
-        name = "randomtorio-randomseed",
-        setting_type = "startup",
-        default_value = 47605
+        order = "c-a"
     },
     {
         type = "int-setting",
         name = "randomtorio-check-amount",
         setting_type = "startup",
         min_value = 1,
-        default_value = 100000
+        default_value = 100000,
+        order = "c-b"
     },
 })
